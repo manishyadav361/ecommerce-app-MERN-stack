dbba64544dccf3d91138921acb869d18723905d0
manishyadav361 <manishyadav361my@gmail.com>
2021-09-12 20:44:39 +0530

    initial-folder-setup

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+function App() {
+  return (
+    <div className="app">
+      <h3>Hello React</h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Reducers/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from "redux";
+import auth from "./Auth";
+const reducers = combineReducers({ auth });
+export default reducers;
